1f988f9a7b0eb2c5b528d531baff52a23542b9c6
DeathRay00 <rahulgadhiraju@gmail.com>
2025-11-08 02:00:51 +0530

    refactor: rename figma folder to Fallback

--- src/supabase/functions/server/kv_store.tsx
+++ src/supabase/functions/server/kv_store.tsx
@@ -9,7 +9,7 @@ CREATE TABLE kv_store_53ddc61c (
 
 // View at https://supabase.com/dashboard/project/mlghfgoxihztuqnxjnca/database/tables
 
-// This file provides a simple key-value interface for storing Figma Make data. It should be adequate for most small-scale use cases.
+// This file provides a simple key-value interface for storing . It should be adequate for most small-scale use cases.
 import { createClient } from "jsr:@supabase/supabase-js@2.49.8";
 
 const client = () => createClient(
